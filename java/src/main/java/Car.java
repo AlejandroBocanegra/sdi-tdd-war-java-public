027af8f744e1a0bdc3403634e6b4f104e8e05095
AkyunaAkish <akyunaakish@gmail.com>
2015-11-20 11:46:57 -0700

    removed answers

--- java/src/main/java/Car.java
+++ java/src/main/java/Car.java
@@ -8,19 +8,15 @@ public class Car{
 
   //Constructor for Car
   public Car(int mpg){
-    this.mpg = mpg;
-    this.gallons = 0;
-    this.odometer = 0;
+
   }
 
   public void fill(int gas){
-    this.gallons += gas;
+
   }
 
   public void drive(int miles){
-    this.gallons -= miles / this.mpg;
-    this.odometer += miles;
-    this.trips.add(miles + " miles");
+
   }
 
 }
